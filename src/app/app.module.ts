4a866b918e278f999032c5fbdaa1f216f2e184b3
Saavedra98 <saavedraerazoc@gmail.com>
2022-12-28 21:13:44 -0500

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { FormularioComponent } from './contactos/formulario/formulario.component';
+import { CampeonatosComponent } from './servicios/campeonatos/campeonatos.component';
+import { HistoriaComponent } from './nosotros/historia/historia.component';
+import { HomeComponent } from './inicio/home/home.component';
+import { ContactosModule } from './contactos/contactos.module';
+import { ServiciosModule } from './servicios/servicios.module';
 import { NosotrosModule } from './nosotros/nosotros.module';
 import { InicioModule } from './inicio/inicio.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
@@ -6,17 +12,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes:Routes=[
+
+  {path:'', component:HomeComponent },
+  {path:'nosotros', component:HistoriaComponent },
+  {path:'servicios', component:CampeonatosComponent },
+  {path:'contactos', component:FormularioComponent },
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UsuariosModule,
     InicioModule,
-    NosotrosModule
+    NosotrosModule,
+    ServiciosModule,
+    ContactosModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
